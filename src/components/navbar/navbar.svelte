8cfd7fa1ffd9228c9e498762697d35bfba75a67c
BESSE PEDRO p2203838 <pedro.besse@etu.univ-lyon1.fr>
2024-02-21 18:16:13 +0100

    Ajout d'une navbar

---
+++ src/components/navbar/navbar.svelte
@@ -0,0 +1,20 @@
+<div class="navbar">
+    <div class="logo">
+        WIK' ANIME
+    </div>
+    <div class="menu">
+        <div class="item">
+            <a href="">Anime list</a>
+        </div>
+        <div class="item">
+            <a href="">Categories</a>
+        </div>
+        <div class="item">
+            <a href="">Add anime</a>
+        </div>
+    </div>
+</div>
+
+<style>
+    @import './navbar.css';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Navbar from "../components/navbar/navbar.svelte";
+</script>
+<Navbar/>
+Recent anime
+
+
+<style>
+h1 {
+    margin-left : 3rem;
+}
+
+</style>
