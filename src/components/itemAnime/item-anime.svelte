fa6169b5d002c51012c0bb66e3c1253ce93152a8
BESSE PEDRO p2203838 <pedro.besse@etu.univ-lyon1.fr>
2024-02-23 11:54:41 +0100

    Ajout des images dans sitem-anime

---
+++ src/components/itemAnime/item-anime.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let name;
+    export let img;
+</script>
+
+<div class="item-anime">
+    <div class="item-img">
+        <img src="/src/assets/img/{img}.webp" alt="aled">
+    </div>
+    
+    <div class="content">
+        <p>{name}</p>
+    </div>
+</div>
+
+<style>
+    @import './itemAnime.css';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,31 @@
 <script>
     import Navbar from "../components/navbar/navbar.svelte";
+    import ItemAnime from "../components/itemAnime/item-anime.svelte";
 </script>
 <Navbar/>
-Recent anime
+<div class="container">
+    <h1>Recent anime</h1> 
+
+    <div class="recent">
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+        <ItemAnime name={"Mashle"} img={"mashle"}/>
+    </div>
+</div>
+
 
 
 <style>
-h1 {
-    margin-left : 3rem;
-}
+    @import url('https://fonts.googleapis.com/css2?family=Anime+Ace&display=swap');
+    .recent {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
 
 </style>
